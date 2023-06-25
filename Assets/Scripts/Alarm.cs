d4c9efb2c7f77617e800b30a6f4a6dcc659cd464
sanchez613 <129397123+sanchez613@users.noreply.github.com>
2023-06-25 23:16:18 +0300

    Init

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private float _volumeUpStep = 0.05f;
+    [SerializeField] private float _volumeDownStep = 0.1f;
+
+    private AudioSource _audioSource;
+    private float _minVolume = 0;
+    private float _maxVolume = 1;
+    Coroutine _volumeUpJob;
+    Coroutine _volumeDownJob;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            if (_volumeDownJob != null)
+                StopCoroutine(_volumeDownJob);
+
+            _audioSource.Play();
+            _volumeUpJob = StartCoroutine(VolumeChange(_volumeUpStep, _maxVolume));
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            if (_volumeUpJob != null)
+                StopCoroutine(_volumeUpJob);
+
+            _volumeDownJob = StartCoroutine(VolumeChange(_volumeDownStep, _minVolume));
+
+            if (_audioSource.volume == 0)
+                _audioSource.Stop();
+        }
+    }
+
+    private IEnumerator VolumeChange(float step, float targetVolume)
+    {
+        var vaitingTime = new WaitForSeconds(1f);
+
+        while (_audioSource.volume != targetVolume)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, targetVolume, step);
+            yield return vaitingTime;
+        }
+    }
+}
